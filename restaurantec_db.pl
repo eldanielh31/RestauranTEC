6ecc8e4927c384dfb71a833387d7994ac66c0ca3
Daniel Brenes <danbg31@gmail.com>
2020-10-06 10:31:04 -0600

    Creando referencia

--- restaurantec_db.pl
+++ restaurantec_db.pl
@@ -1,44 +1,46 @@
 %Restaurantes disponibles en la app
 %formato: [nombre, tipoMenu, [menu], [direccion], [capacidad], [disposiciones]]
-restaurante(mcdonald, comida_rapida, [sanjose, 'metrocentro'], 4, 'Uso de mascarilla obligatorio').
-restaurante(pizzahut, italiana, [cartago, 'ruinar'], 5, 'Uso de mascarilla obligatorio').
-restaurante(tejarena, comida_rapida, [cartago, 'tejar'], 10, 'Uso de mascarilla obligatorio').
-restaurante(woods, italiana, [sanjose, 'curridabat'], 20, 'Uso de mascarilla obligatorio').
-
-%Ejercicio5 (miembro en listas)
-miembro(X,[X|_]).
-miembro(X,[_|T]):-miembro(X,T).
+Terrestaurante(mcdonald, comida_rapida, [sanjose, 'metrocentro'], 4, 'el uso de mascarilla obligatorio').
+restaurante(pizzahut, italiana, [cartago, 'ruinar'], 5, 'el uso de mascarilla obligatorio').
+restaurante(tejarena, comida_rapida, [cartago, 'tejar'], 10, 'el uso de mascarilla obligatorio').
+restaurante(woods, italiana, [sanjose, 'curridabat'], 20, 'el uso de mascarilla obligatorio').
 
 %tipos menu
 %formato: (tipo comida, [sabores])
-menu(hamburguesas, mcdonald, [conQueso, dobleTorta, vegana]).
-menu(hamburguesas, tejarena, [conQueso, dobleTorta, vegana]).
+menu(hamburguesas, [mcdonald, tejarena], [conQueso, dobleTorta, vegana]).
 
-menu(pizza, pizzahut, [hongos, jamon]).
-menu(pizza, woods, [hongos, jamon, hawaiana]).
+menu(pizza, [pizzahut], [hongos, jamon]).
+menu(pizza, [woods], [hongos, jamon, hawaiana]).
 
-menu(calzone, pizzahut, [tomate, pina]).
-menu(calzone, woods, [tomate, pina, remolacha]).
+menu(calzone, [pizzahut, woods], [tomate, pina]).
 
-menu(papas, mcdonald, [grandes, pequenas]).
-menu(papas, tejarena, [grandes, pequenas]).
+menu(papas, [mcdonald, tejarena], [grandes, pequenas]).
 
-menu(refresco, woods, [cocaCola, sevenUp, uva]).
-menu(refresco, pizzahut, [cocaCola, sevenUp, uva]).
-menu(refresco, mcdonald, [cocaCola, sevenUp, uva]).
-menu(refresco, tejarena, [cocaCola, sevenUp, uva]).
+menu(refresco, [woods, pizzahut, mcdonald, tejarena], [cocaCola, sevenUp, uva]).
 
-buscarResta(TipoMenu, Comida, TipoComida, Lugar, Capacidad, N, D):-
-    restaurante(Nombre, TipoMenu, [Lugar | Direccion], Capacidad, Obligaciones),
-    menu(Comida, Nombre, T),
+%aperaciones basicas
+miembro(X,[X|_]).
+miembro(X,[_|T]):-miembro(X,T).
+
+%busca el mejor restaurante segun los parametros dados.
+buscarResta(TipoMenu, Comida, TipoComida, Lugar, Capacidad):-
+    restaurante(Nombre, TipoMenu, [Lugar | Direccion], RCapacidad, Obligaciones),
+    Capacidad =< RCapacidad,
+    menu(Comida, LNombre, T),
+    miembro(Nombre, LNombre),
     miembro(TipoComida, T),
-    N = Nombre,
-    D = Direccion.
+    crearReferencia(Nombre, Direccion, Obligaciones), !.
+
+crearReferencia(Nombre, Direccion, Obligaciones):-
+    write('Nuestra sugerencia es '),
+    write(Nombre),
+    write( '. Su direccion es: '),
+    write(Direccion),
+    write('. Tenga en cuenta que para ingresar debe tener en cuenta que '),
+    write(Obligaciones).
 
 inicio():-
     %write('Escribe'),
     %nl,
     %read(A),
-    buscarResta(comida_rapida , hamburguesas, conQueso, cartago, 10, Nombre, Direccion),
-    write(Nombre),nl,
-    write(Direccion).
+    buscarResta(comida_rapida , hamburguesas, conQueso, cartago, 10),
--- test2.pl
+++ test2.pl
@@ -1,30 +1,7 @@
-
-%Restaurantes disponibles en la app
-%formato: [nombre, tipoMenu,[direccion], [capacidad], [disposiciones]]
-restaurante(mcdonald, comida_rapida, [cartago, 'metrocentro'], 30, 'Uso de mascarilla obligatorio').
-
-%tipos menu
-%formato: (tipo comida, [sabores])
-menu(hamburguesas, mcdonald).
-menu(hamburguesas, tejarena).
-
-menu(papas, mcdonald).
-menu(papas, bk).
-
-menu(refresco, mcdonald).
-
-hamburguesas('con queso', mcdonald).
-
-buscarResta(TipoMenu, Comida, Lugar, Capacidad, N, D):-
-    restaurante(Nombre, TipoMenu, [Lugar | Direccion], Capacidad, Obligaciones),
-    menu(Comida, Nombre),
-    N = Nombre,
-    D = Direccion.
-
-inicio():-
-    write('Escribe'),
-    nl,
-    read(A),
-    buscarResta(A, hamburguesas, cartago, 30, Nombre, Direccion),
-    write(Nombre),nl,
-    write(Direccion).
+crearReferencia(Nombre, Direccion, Obligaciones):-
+    write('Nuestra sugerencia es '),
+    write(Nombre),
+    write( '. Su direccion es: '),
+    write(Direccion),
+    write('. Tenga en cuenta que para ingresar debe tener en cuenta que '),
+    write(Obligaciones).
