45b740e1e4df976811f3fdaa13f088fad9049525
Daniel Brenes <danbg31@gmail.com>
2020-10-10 11:47:56 -0600

    cambio rapida

--- restaurantec_db.pl
+++ restaurantec_db.pl
@@ -2,66 +2,66 @@
 %formato: [nombre, tipoMenu, [direccion], [capacidad], [disposiciones]]
 
 %San Jose
-restaurante(mcdonald, comida_rapida, [sanjose, 'Plaza del Sol'], 30, 'el uso de mascarilla obligatorio').
+restaurante(mcdonald, rapida, [sanjose, 'Plaza del Sol'], 30, 'el uso de mascarilla obligatorio').
 restaurante(woods, italiana, [sanjose, 'Curridabat'], 20, 'el uso de mascarilla obligatorio').
 restaurante(tacobell, mexicana, [sanjose, 'San Pedro'], 30, 'el uso de mascarilla obligatorio').
-restaurante(kfc, comida_rapida,[sanjose, 'San Pedro'],20,'el uso de mascarilla obligatorio').
+restaurante(kfc, rapida,[sanjose, 'San Pedro'],20,'el uso de mascarilla obligatorio').
 restaurante(marisqueando, mariscos,[sanjose, 'Centro de Desamparados'],20,'el uso de mascarilla obligatorio').
 restaurante(hongkong,china,[sanjose, 'Moravia'],20,'el uso de mascarilla obligatorio').
 restaurante(yokohama, japonesa, [sanjose, '200 mts al oeste de la Universidad de Costa Rica'], 20, 'el uso de mascarilla obligatorio').
 
 %Cartaguito campeon
 restaurante(autogrill, bar, [cartago, 'Mall Paseo Metropoli'], 40, 'el uso de mascarilla obligatorio').
-restaurante(mcdonald, comida_rapida, [cartago, 'MetroCentro'], 30, 'el uso de mascarilla obligatorio').
+restaurante(mcdonald, rapida, [cartago, 'MetroCentro'], 30, 'el uso de mascarilla obligatorio').
 restaurante(pizzahut, italiana, [cartago, 'ruinas'], 5, 'el uso de mascarilla obligatorio').
-restaurante(tejarena, comida_rapida, [cartago, 'tejar'], 10, 'el uso de mascarilla obligatorio').
+restaurante(tejarena, rapida, [cartago, 'tejar'], 10, 'el uso de mascarilla obligatorio').
 restaurante(tacobell, mexicana, [cartago, '100mts norte de las Ruinas'], 30, 'el uso de mascarilla obligatorio').
 restaurante(yokohama, japonesa, [cartago, 'Plaza Boulevard, Blvd. el Molino, Provincia de Cartago, Cartago'], 20, 'el uso de mascarilla obligatorio').
-restaurante(kfc, comida_rapida,[cartago, '50 mts al sur de las Ruinas'],20,'el uso de mascarilla obligatorio').
+restaurante(kfc, rapida,[cartago, '50 mts al sur de las Ruinas'],20,'el uso de mascarilla obligatorio').
 restaurante(linfei,china,[cartago, '200 mts al este de la Basilica de Los Angeles'],20,'el uso de mascarilla obligatorio').
 
 %Puntarenas
-restaurante(mcdonald, comida_rapida, [puntarenas, 'Plaza Centenario'], 30, 'el uso de mascarilla obligatorio').
-restaurante(rostipollos, comida_rapida, [puntarenas, 'Hotel Alamar'], 30, 'el uso de mascarilla obligatorio').
+restaurante(mcdonald, rapida, [puntarenas, 'Plaza Centenario'], 30, 'el uso de mascarilla obligatorio').
+restaurante(rostipollos, rapida, [puntarenas, 'Hotel Alamar'], 30, 'el uso de mascarilla obligatorio').
 restaurante(pizzahut, italiana, [puntarenas, 'Ocean Mall'], 20, 'el uso de mascarilla obligatorio').
 restaurante(fishy, mariscos, [puntarenas, 'Parque Mora'], 30, 'el uso de mascarilla obligatorio').
-restaurante(kfc, comida_rapida,[puntarenas, 'Puntarenas Down Town'],20,'el uso de mascarilla obligatorio').
+restaurante(kfc, rapida,[puntarenas, 'Puntarenas Down Town'],20,'el uso de mascarilla obligatorio').
 restaurante(yokohama, japonesa, [puntarenas, 'Puntarenas Down Town'],20,'el uso de mascarilla obligatorio').
 restaurante(tacobell, mexicana, [puntarenas, 'Plaza La Rioja'], 30, 'el uso de mascarilla obligatorio').
 restaurante(wingshun,china,[puntarenas, 'Centro de la capital'],20,'el uso de mascarilla obligatorio').
 
 %Heredia
-restaurante(mcdonald, comida_rapida, [herjkedia, 'Oxigeno Human Playground'], 30, 'el uso de mascarilla obligatorio').
+restaurante(mcdonald, rapida, [herjkedia, 'Oxigeno Human Playground'], 30, 'el uso de mascarilla obligatorio').
 restaurante(pizzahut, italiana, [heredia, 'Mall Paseo de Las Flores'], 20, 'el uso de mascarilla obligatorio').
 restaurante(mariscosymas, mariscos, [heredia, 'Entrada principal de la UNA 100 oeste y 25 Sur'], 30, 'el uso de mascarilla obligatorio').
-restaurante(kfc, comida_rapida,[heredia, 'Mall Paseo de Las Flores'],20,'el uso de mascarilla obligatorio').
+restaurante(kfc, rapida,[heredia, 'Mall Paseo de Las Flores'],20,'el uso de mascarilla obligatorio').
 restaurante(yokohama, japonesa, [heredia, 'Contiguo Al AYA De San Pablo 112, Heredia, San Pablo'],20,'el uso de mascarilla obligatorio').
 restaurante(tacobell, mexicana, [heredia, 'Mall Paseo de Las Flores'], 30, 'el uso de mascarilla obligatorio').
 restaurante(lungfung,china,[heredia, 'Limón, Heredia'],20,'el uso de mascarilla obligatorio').
 
 %Alajuela
-restaurante(mcdonald, comida_rapida, [alajuela, 'Diagonal Bomba Provincia de Alajuela La Tropicana, Provincia de Alajuela'], 30, 'el uso de mascarilla obligatorio').
+restaurante(mcdonald, rapida, [alajuela, 'Diagonal Bomba Provincia de Alajuela La Tropicana, Provincia de Alajuela'], 30, 'el uso de mascarilla obligatorio').
 restaurante(pizzahut, italiana, [alajuela, 'Provincia de Alajuela, Alajuela'], 20, 'el uso de mascarilla obligatorio').
 restaurante(limonta, mariscos, [alajuela, 'Avenida 6 y calle 5, casa esquinera mano derecha Alajuela'], 30, 'el uso de mascarilla obligatorio').
-restaurante(kfc, comida_rapida,[alajuela, 'Cruce calle 2 Obispo Tristán y avenida 10 Jesús Ocaña'],20,'el uso de mascarilla obligatorio').
+restaurante(kfc, rapida,[alajuela, 'Cruce calle 2 Obispo Tristán y avenida 10 Jesús Ocaña'],20,'el uso de mascarilla obligatorio').
 restaurante(matsuri, japonesa, [alajuela, 'Tropicana, Provincia de Alajuela, Alajuela'],20,'el uso de mascarilla obligatorio').
 restaurante(tacobell, mexicana, [alajuela, 'Provincia de Alajuela, Alajuela'], 30, 'el uso de mascarilla obligatorio').
 restaurante(meywah,china,[alajuela, 'Av. 3 Tomás Guardia, Provincia de Alajuela, Alajuela'],20,'el uso de mascarilla obligatorio').
 
 %Guanacaste
-restaurante(mcdonald, comida_rapida, [guanacaste, 'Carretera Interamericana Esq, Av. Central, Provincia de Guanacaste, Liberia'], 30, 'el uso de mascarilla obligatorio').
+restaurante(mcdonald, rapida, [guanacaste, 'Carretera Interamericana Esq, Av. Central, Provincia de Guanacaste, Liberia'], 30, 'el uso de mascarilla obligatorio').
 restaurante(pizzahut, italiana, [guanacaste, 'Provincia de Guanacaste, Santa Cruz'], 20, 'el uso de mascarilla obligatorio').
 restaurante(reina, mariscos, [guanacaste, 'Provincia de Guanacaste, Curime'], 30, 'el uso de mascarilla obligatorio').
-restaurante(kfc, comida_rapida,[guanacaste, 'Diagonal a Plaza Santa Rosa, Liberia, Guanacaste, Provincia de Guanacaste, Liberia'],20,'el uso de mascarilla obligatorio').
+restaurante(kfc, rapida,[guanacaste, 'Diagonal a Plaza Santa Rosa, Liberia, Guanacaste, Provincia de Guanacaste, Liberia'],20,'el uso de mascarilla obligatorio').
 restaurante(cos-ita, japonesa, [guanacaste, 'Parada de Buses Samara - San Jose, 160, Provincia de Guanacaste, Sámara'],20,'el uso de mascarilla obligatorio').
 restaurante(tacobell, mexicana, [guanacaste, 'Limonal, Provincia de Guanacaste, La Palma'], 30, 'el uso de mascarilla obligatorio').
 restaurante(yongxin,china,[guanacaste, 'Provincia de Guanacaste, Liberia'],20,'el uso de mascarilla obligatorio').
 
 %Limon
-restaurante(mcdonald, comida_rapida, [limon, 'Calle 8, Limon'], 30, 'el uso de mascarilla obligatorio').
+restaurante(mcdonald, rapida, [limon, 'Calle 8, Limon'], 30, 'el uso de mascarilla obligatorio').
 restaurante(pizzahut, italiana, [limon, 'Av 2, Limón'], 20, 'el uso de mascarilla obligatorio').
 restaurante(rancho, mariscos, [limon, 'Unnamed Road, Liman, Guapiles'], 30, 'el uso de mascarilla obligatorio').
-restaurante(kfc, comida_rapida,[limon, 'Centro de Limon'],20,'el uso de mascarilla obligatorio').
+restaurante(kfc, rapida,[limon, 'Centro de Limon'],20,'el uso de mascarilla obligatorio').
 restaurante(tacobell, mexicana, [limon, 'Carr Braulio Carrillo, Limon, Guapiles'], 30, 'el uso de mascarilla obligatorio').
 restaurante(china-garden,china,[limon, 'Pocora, Limon, sobre ruta 32, 100 este de Almacenes El Colono de, Limon, Pocora'],20,'el uso de mascarilla obligatorio').
 
