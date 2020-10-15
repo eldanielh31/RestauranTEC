cbb7403c89a5ee2d3621f174874dac9844e5e0c4
04esteban04 <estebanmadrigal7@gmail.com>
2020-10-14 19:46:45 -0600

    Finalizando archivos

--- restaurantecSE.pl
+++ restaurantecSE.pl
@@ -1,4 +1,4 @@
-% Se importan los archivos que contiene la base de datos y el BNF
+% Se importan los archivos que contienen la base de datos y el BNF
 :-consult('restaurantecDataBase').
 :-consult('restaurantecBNF').
 :-style_check(-singleton).
@@ -7,11 +7,10 @@
 %***********************    Operaciones basicas    ****************************
 %******************************************************************************
 
-
-%  Descripción		:	Determinantes masculinos
-% Nombre de Hecho	:	determinante_m(X)
-% Parámetro			:	determinantes masculinos
-% Uso				:	sintagma_nominal([A],[B])
+% Descripción		:	Se verifica si una lista esta vacia
+% Nombre de Hecho	:	lista_vacia([],X)
+% Parámetro			:	lista a verificar, resultado
+% Salida            :   boolean que indica si la lista esta vacia o no
 lista_vacia(List, Empty) :-
     length(List, Len),
     (   Len =< 1
@@ -19,22 +18,42 @@ lista_vacia(List, Empty) :-
     ;   Empty = false
     ).
 
+% Descripción		:	Se convierte la entrada a lista
+% Nombre de Hecho	:	input_to_list(X)
+% Parámetro			:	resultado
+% Salida            :   entrada convertida en lista
 input_to_list(L):-
 	read_line_to_codes(user_input,Cs),
 	atom_codes(A,Cs),
 	atomic_list_concat(L,' ',A).
 
+% Descripción		:	Se convierte la entrada a string
+% Nombre de Hecho	:	input_to_string(X)
+% Parámetro			:	resultado
+% Salida            :   entrada convertida en string
 input_to_string(A):-
 	read_line_to_codes(user_input,Cs),
 	atom_codes(A,Cs).
 
+% Descripción		:	Se convierte la entrada a string
+% Nombre de Hecho	:	list_to_string([],X)
+% Parámetro			:	lista, resultado
+% Salida            :   lista ingresada convertida en string
 list_to_string(List, String):-
 	atomic_list_concat(List, ' ', String).
 
+% Descripción		:	Se convierte la entrada a lista
+% Nombre de Hecho	:	string_to_list_of_atoms(X,[])
+% Parámetro			:	string, resultado
+% Salida            :   entrada convertida en lista
 string_to_list_of_atoms(X,L):-
 	atom_codes(X,A),
 	atomic_list_concat(L,' ',X).
 
+% Descripción		:	Se verifica si un elemento pertenece a la lista ingresada
+% Nombre de Hecho	:	miembro(X,[])
+% Parámetro			:	elemento, lista
+% Salida            :   boolean que indica si el elemento pertenece a la lista dada
 miembro(X,[X|_]).
 miembro(X,[_|T]):-miembro(X,T).
 
@@ -52,9 +71,10 @@ miembro(X,[_|T]):-miembro(X,T).
 %******************************************************************************
 
 
-% Se busca el tipo de menu
-listaTipoDeMenu(L) :- findall(X, (restaurante([_,X,_,_,_])) , L).
-
+% Descripción		:	Se busca el tipo de menu
+% Nombre de Hecho	:	compareTipoDeMenu([], X)
+% Parámetro			:	lista, resultado
+% Salida            :   nombre del menu 
 compareTipoDeMenu([],X):- nl, writeln('El tipo de menu ingresado no esta disponible'),
 						  writeln('Intente de nuevo'),nl,
 						  input_to_list(Oracion),
@@ -66,9 +86,16 @@ compareTipoDeMenu([H|T], X):- listaTipoDeMenu(L),
 	                          \+miembro(H,L),
 	                          compareTipoDeMenu(T,X).
 
-% Se buscan los restaurantes 
-listaRestaurantes(L) :- findall(X, (restaurante([X|_])), L).
+% Descripción		:	Se obtiene la lista de menus disponibles
+% Nombre de Hecho	:	listaTipoDeMenu(L)
+% Parámetro			:	resultado
+% Salida            :   lista de menus disponibles
+listaTipoDeMenu(L) :- findall(X, (restaurante([_,X,_,_,_])) , L).
 
+% Descripción		:	Se buscan los restaurantes 
+% Nombre de Hecho	:	compareRest([], X)
+% Parámetro			:	lista, resultado
+% Salida            :   nombre del restaurante 
 compareRest([],X):- nl, writeln('El restaurante ingresado no esta disponible'),
 					writeln('Intente de nuevo'),nl,
 					input_to_list(Oracion),
@@ -80,9 +107,16 @@ compareRest([H|T], X):- listaRestaurantes(L),
 	                    \+miembro(H,L),
 	                    compareRest(T,X).
 
-% Se busca la comida
-listaComidas(L) :- findall(X, (menu([X|_])), L).
+% Descripción		:	Se obtiene la lista de restaurantes disponibles
+% Nombre de Hecho	:	listaRestaurantes(L)
+% Parámetro			:	resultado
+% Salida            :   lista de restaurantes disponibles
+listaRestaurantes(L) :- findall(X, (restaurante([X|_])), L).
 
+% Descripción		:	Se busca la comida
+% Nombre de Hecho	:	compareComida([], X)
+% Parámetro			:	lista, resultado
+% Salida            :   nombre de la comida 
 compareComida([],X):- nl, writeln('La comida ingresada no esta disponible'),
 					  writeln('Intente de nuevo'),nl,
 					  input_to_list(Oracion),
@@ -94,9 +128,16 @@ compareComida([H|T], X):- listaComidas(L),
 	                      \+miembro(H,L),
 	                      compareComida(T,X).
 
-% Se buscan el sabor especifico de la comida
-listaSaborComida(L) :- findall(X, (menu([_,_,X])), L).
+% Descripción		:	Se obtiene la lista de comidas disponibles
+% Nombre de Hecho	:	listaComidas(L)
+% Parámetro			:	resultado
+% Salida            :   lista de comidas disponibles
+listaComidas(L) :- findall(X, (menu([X|_])), L).
 
+% Descripción		:	Se busca el sabor especifico de la comida
+% Nombre de Hecho	:	compareSaborComida([], X)
+% Parámetro			:	lista, resultado
+% Salida            :   nombre del sabor de la comida 
 compareSaborComidaAux([H|_], X):- listaSaborComida(L),
 						          flatten(L,Y),
 						          X = Y.
@@ -112,9 +153,16 @@ compareSaborComida([H|T], X):- compareSaborComidaAux(C, Y),
 	                           \+miembro(H,Y),
 	                           compareSaborComida(T,X).
 
-% Se buscan los refrescos
-listaBebidas(L) :- findall(X, (bebida([X|_])) , L).
+% Descripción		:	Se obtiene la lista de sabores de comida disponibles
+% Nombre de Hecho	:	listaSaborComida(L)
+% Parámetro			:	resultado
+% Salida            :   lista de sabores de comida disponibles
+listaSaborComida(L) :- findall(X, (menu([_,_,X])), L).
 
+% Descripción		:	Se buscan los refrescos
+% Nombre de Hecho	:	compareBebida([], X)
+% Parámetro			:	lista, resultado
+% Salida            :   nombre del sabor de la bebida
 compareBebida([],X):- nl, writeln('El sabor de la bebida ingresada no esta disponible'),
 					  writeln('Intente de nuevo'),nl,
 					  input_to_list(Oracion),
@@ -126,9 +174,16 @@ compareBebida([H|T], X):- listaBebidas(L),
 	                      \+miembro(H,L),
 	                      compareBebida(T,X).
 
-% Se buscan los lugares
-listaLugar(L) :- findall(X, (restaurante([_,_,X,_,_])) , L).
+% Descripción		:	Se obtiene la lista de bebidas disponibles
+% Nombre de Hecho	:	listaBebidas(L)
+% Parámetro			:	resultado
+% Salida            :   lista de bebidas disponibles
+listaBebidas(L) :- findall(X, (bebida([X|_])) , L).
 
+% Descripción		:	Se buscan los lugares
+% Nombre de Hecho	:	compareLugar([], X)
+% Parámetro			:	lista, resultado
+% Salida            :   nombre del lugar
 compareLugarAux([H|_], X):- listaLugar(L),
 						    flatten(L,Y),
 						    X = Y.
@@ -144,11 +199,16 @@ compareLugar([H|T], X):- compareLugarAux(C, Y),
 	                     \+miembro(H,Y),
 	                     compareLugar(T,X).
 
-% Se busca la cantidad
-listaCantidad(Cantidad) :- numlist(1, 40, CantidadTemp),
-							list_to_string(CantidadTemp, Str),
-							string_to_list_of_atoms(Str,Cantidad).	
+% Descripción		:	Se obtiene la lista de lugares disponibles
+% Nombre de Hecho	:	listaLugar(L)
+% Parámetro			:	resultado
+% Salida            :   lista de lugares disponibles
+listaLugar(L) :- findall(X, (restaurante([_,_,X,_,_])) , L).
 
+% Descripción		:	Se busca la cantidad
+% Nombre de Hecho	:	compareCantidad([], X)
+% Parámetro			:	lista, resultado
+% Salida            :   cantidad deseada de personas
 compareCantidad([],X):-  nl, writeln('No hay cupo para la cantidad de personas ingresadas'),
 						 writeln('Intente de nuevo'),nl,
 						 input_to_list(Oracion),
@@ -161,10 +221,21 @@ compareCantidad([H|T], X):- listaCantidad(L),
 	                        \+miembro(H,L),
 	                        compareCantidad(T,X).
 
+% Descripción		:	Se obtiene la lista de cantidades disponibles
+% Nombre de Hecho	:	listaCantidad(L)
+% Parámetro			:	resultado
+% Salida            :   lista de cantidades disponibles
+listaCantidad(Cantidad) :- numlist(1, 40, CantidadTemp),
+							list_to_string(CantidadTemp, Str),
+							string_to_list_of_atoms(Str,Cantidad).	
 
+% Descripción		:	Se piden los datos al usuario
+% Nombre de Hecho	:	pedirDatos(NombreRest, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDeseado, CantidadDeseada)
+% Parámetro			:	NombreRestaurante, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDeseado, CantidadDeseada
+% Salida            :   resultado de buscar los datos ingresados en la base de datos
 pedirDatos(NombreRest, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDeseado, CantidadDeseada):- 
 	% Se busca el tipo de menu
-	nl, writeln('¿Que tipo de menu desea comer? (Puede se rapida, china, etc.)'),
+	nl, writeln('¿Que tipo de menu desea comer? (Puede ser rapida, china, etc.)'),
 	input_to_list(Oracion),
 	validacion_gramatical(Oracion),
     compareTipoDeMenu(Oracion, TipoMenuTemp),
@@ -215,13 +286,19 @@ pedirDatos(NombreRest, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDesea
 	% Se validan los datos y se busca la referencia en caso de existir
 	buscarRestauranteConDatosIngresados(NombreRest, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDeseado, CantidadDeseada).
 
-
+% Descripción		:	Se verifican los datos ingresados por el usuario
+% Nombre de Hecho	:	verificarDatos(NombreRest, NombreIngresado, NombreIngresado2, SaborComida, SaboresDisponibles)
+% Parámetro			:	NombreRestaurante, NombreIngresado, NombreIngresado2, SaborComida, SaboresDisponibles
+% Salida            :   boolean que indica si los datos se encuentran en la base de datos o no
 verificarDatos(NombreRest, NombreIngresado, NombreIngresado2, SaborComida, SaboresDisponibles):-	
 	miembro(NombreRest, NombreIngresado), 
 	miembro(NombreRest, NombreIngresado2), 
 	miembro(SaborComida, SaboresDisponibles).
 
-%busca el mejor restaurante segun los parametros dados.
+% Descripción		:	Busca el mejor restaurante segun los parametros dados.
+% Nombre de Hecho	:	buscarRestauranteConDatosIngresados(NombreRest, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDeseado, CantidadDeseada)
+% Parámetro			:	NombreRestaurante, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDeseado, CantidadDeseada
+% Salida            :   referencia generada si se encuentran los datos ingresados
 buscarRestauranteConDatosIngresados(NombreRest, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDeseado, CantidadDeseada):-
     restaurante([NombreRest, TipoMenu, [LugarDeseado | Direccion], RCapacidad, Obligaciones]),
 	CantidadDeseada > 0,
@@ -244,27 +321,35 @@ buscarRestauranteConDatosIngresados(NombreRest, TipoMenu, TipoComida, SaborComid
 	nl, writeln('**************************************************************************************'), nl,
 	buscarNuevamente(), !.
 
+% Descripción		:	Se crea la referencia para el mejor restaurante segun los parametros dados.
+% Nombre de Hecho	:	crearReferencia(NombreRest, DireccionTemp, Obligaciones)
+% Parámetro			:	NombreRestaurante, DireccionTemporal, Obligaciones
+% Salida            :   referencia generada si se encuentran los datos ingresados
 crearReferencia(NombreRest, DireccionTemp, Obligaciones):-
 	list_to_string(DireccionTemp,Direccion),
-	%TempRef = [],
 
-	%list_to_string(TempRef, Ref), nl,
 	nl, writeln('*******************************************************************************************'), nl,
 	write('--> Nuestra sugerencia de restaurante es: '), write(NombreRest), writeln(' <--'), 
 	write('--> La direccion es: '), write(Direccion), writeln(' <--'), 
 	write('--> Tenga en cuenta que para ingresar al restaurante '), write(Obligaciones), writeln(' <--'),
 	nl, writeln('*******************************************************************************************'), nl.
 
+% Descripción		:	Se pregunta al usuario si desea buscar nuevamente.
+% Nombre de Hecho	:	buscarNuevamenteAux(Respuesta)
+% Parámetro			:	respuesta del usuario si desea buscar nuevamente o no
+% Salida            :   mensaje de despedida en caso de no querer volver a buscar
 buscarNuevamenteAux(Respuesta):- miembro('si',Respuesta),
 								 pedirDatos(A,B,C,D,E,F,G).
 buscarNuevamenteAux(Respuesta):- \+miembro('si',Respuesta),
 								 despedida(), !.
-
 buscarNuevamente():- nl, writeln('--> Desea buscar nuevamente? (Debe responder con si o no a la pregunta) <--'), nl,
 						input_to_list(Respuesta),
 						buscarNuevamenteAux(Respuesta), !.
 
-
+% Descripción		:	Se muestra el mensaje de bienvenida al usuario.
+% Nombre de Hecho	:	encabezado()
+% Parámetro			:	-
+% Salida            :   mensaje de bienvenida al usuario
 encabezado():-
 	sleep(0.02),
 	write('       ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||       '),nl,
@@ -279,11 +364,18 @@ encabezado():-
 	write('          Bienvenido(a)(os)(as) a RestauranTEC, es un placer ayudarle(s) '), nl,nl.
 
 
-
+% Descripción		:	Se muestra el mensaje de despedida al usuario.
+% Nombre de Hecho	:	despedida()
+% Parámetro			:	-
+% Salida            :   mensaje de despedida al usuario
 despedida():- nl, writeln('**************************************************************************************'), nl,
 			  nl, write('         --> Muchas gracias por preferirnos. '), write('Lo esperamos pronto. <--'), nl, nl,
 			  nl, writeln('**************************************************************************************'), nl, !. 
 
+% Descripción		:	Regla principal que ejecuta el programa.
+% Nombre de Hecho	:	inicio()
+% Parámetro			:	-
+% Salida            :   -
 inicio():-
 	encabezado(),
 	pedirDatos(A,B,C,D,E,F,G).
